d721fe89de8a6aab0c29531681619319a914e94b
Galentin <atapol89@mail.ru>
2017-09-25 19:50:00 +0300

    Исправление

--- src/Subarray/Subarray.java
+++ src/Subarray/Subarray.java
@@ -6,7 +6,7 @@ public class Subarray {
         int sum = 0;
         int leftIndex = 0;
         int currentLeftIndex = 0;
-        int rightIndex = 0;
+        int rightIndex = -1;
 
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
@@ -15,12 +15,13 @@ public class Subarray {
                 leftIndex = currentLeftIndex;
                 rightIndex = i;
             } else if (sum < 0) {
-                currentLeftIndex = i;
+                currentLeftIndex = i + 1;
                 sum = 0;
             }
         }
         int[] result;
-        if (leftIndex == rightIndex) result = new int[]{leftIndex};
+        if (rightIndex == -1) result = new int[] {};
+        else if (leftIndex == rightIndex) result = new int[]{leftIndex};
         else result = new int[]{leftIndex, rightIndex};
         return result;
     }
--- test/Subarray/SubarrayTest.java
+++ test/Subarray/SubarrayTest.java
@@ -7,14 +7,22 @@ import static org.junit.Assert.*;
 public class SubarrayTest {
     @Test
     public void maxSubarray() throws Exception {
-        int[] a = new int[] {2,-4, 3, 5, 2, -7,1};
-        int[] b = new int[] {1,4};
+        int[] a = new int[] {2, -4, 3, 5, 2, -7, 1};
+        int[] b = new int[] {2, 4};
 
-        int[] a2 = new int[] {2,-4, -3, -5, -2, -7,-1};
+        int[] a2 = new int[] {2, -4, -3, -5, -2, -7, -1};
         int[] b2 = new int[] {0};
+
+        int[] a3 = new int[] {2, 1, 4, 6, 8, 7, 1};
+        int[] b3 = new int[] {0, 6};
+
+        int[] a4 = new int[] {-2, -1, -4, -6, -8, -7, -1};
+        int[] b4 = new int[] {};
         Subarray mas = new Subarray();
         assertArrayEquals(b, mas.maxSubarray(a));
         assertArrayEquals(b2, mas.maxSubarray(a2));
+        assertArrayEquals(b3, mas.maxSubarray(a3));
+        assertArrayEquals(b4, mas.maxSubarray(a4));
     }
 
 }
